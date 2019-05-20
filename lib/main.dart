f0f94918af2b677d8b5cff79fcbe6262be37f65e
Nihal <geekram13@gmail.com>
2019-05-20 22:51:27 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    Center(
+      child: Text(
+        "My First App",
+        textDirection: TextDirection.ltr,
+
+      ),
+    )
+  );
+}
